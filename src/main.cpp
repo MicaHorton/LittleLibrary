c2418e969a916c1b1720a1345739b72dbd79cbbf
Mica Horton <mica.e.horton@gmail.com>
2020-04-05 16:47:56 -0700

    Fix it so buttons and books counter work

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,79 @@
 #include <Arduino.h>
-#include "LowPower.h"
 
-int const inButtonPin = 3;
-int inButtonState = 0;
-int inLastButtonState = 1;
-int const inLEDPin = 4;
+int const inButton = 7;
+int inState = 0;
+int inLastState = 1;
+int const inLED = 8;
 
-int const outButtonPin = 0;
-int outButtonState = 0;
-int outLastButtonState = 1;
-int const outLEDPin = 1;
+int const outButton = 11;
+int outState = 0;
+int outLastState = 1;
+int const outLED = 12;
 
 int books = 0;
-unsigned long previousCountMillis = 0; 
-const long countInterval = 100; 
+unsigned long previousMillis = 0;        
+const long interval = 100; 
 
 
 void setup() {
-  pinMode(inButtonPin, INPUT_PULLUP);
-  pinMode(outButtonPin, INPUT_PULLUP);
-  pinMode(inLEDPin, OUTPUT);
-  pinMode(outLEDPin, OUTPUT);
+  pinMode(inButton, INPUT_PULLUP);
+  pinMode(outButton, INPUT_PULLUP);
+  pinMode(inLED, OUTPUT);
+  pinMode(outLED, OUTPUT); 
   Serial.begin(9600);
 }
 
 void loop() {
+  inState = digitalRead(inButton);
+  outState = digitalRead(outButton);
 
-  //If in button is has been pushed, activate the LED and increase number of books.
-  //If out button has been pushed, activate the LED and decrease number of books.
-  outButtonState = digitalRead(outButtonPin);
-  inButtonState = digitalRead(inButtonPin);
+  unsigned long currentMillis = millis();
+ 
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    Serial.println(books); 
+
+    if (inState == LOW) {
+      digitalWrite(inLED, HIGH);
+      if (inState != inLastState) books++;
+    } else {
+      digitalWrite(inLED, LOW);
+    };
+    
+    if (outState == LOW) {
+      digitalWrite(outLED, HIGH);
+      if (outState != outLastState) books--;
+    } else {
+      digitalWrite(outLED, LOW);
+    };
+
+    inLastState = inState;
+    outLastState = outState; 
+
+  }; 
+
   
-  //Print amount of books out if proper interval of time has passed.
-  unsigned long currentCountMillis = millis();
-  if (currentCountMillis - previousCountMillis >= countInterval) {
-    previousCountMillis = currentCountMillis;
-    Serial.println(books);
-    if (outButtonState == LOW) {
-      if (outButtonState != outLastButtonState) books--;
-      digitalWrite(outLEDPin, HIGH);
-    } else {
-      digitalWrite(outLEDPin, LOW);
-    }
-    outLastButtonState = outButtonState;
-    if (inButtonState == LOW) {
-      if (inButtonState != inLastButtonState) books++;
-      digitalWrite(inLEDPin, HIGH);
-    } else {
-      digitalWrite(inLEDPin, LOW);
-    }
-    inLastButtonState = inButtonState;
-  }
+}
 
 
+
+
+
+/* Testing Code
+
+int const inButton = 4;
+int inState = 0;
+
+void setup() {
+  pinMode(inButton, INPUT_PULLUP);
+  Serial.begin(9600);
 
 }
+
+void loop() {
+  inState = digitalRead(inButton);
+  Serial.println(inState);
+
+}
+
+*/
